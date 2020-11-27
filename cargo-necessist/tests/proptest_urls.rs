5b98816e6cad7fd77abe323de23313912c844bcb
Samuel Moelius <sam.moelius@trailofbits.com>
2020-11-27 21:24:03 +0000

    Handle ssh urls

--- cargo-necessist/tests/proptest_urls.rs
+++ cargo-necessist/tests/proptest_urls.rs
@@ -2,6 +2,12 @@ use assert_cmd::prelude::*;
 use std::process::Command;
 
 #[test]
-fn proptest_urls() {
-    Command::new("tests/proptest_urls.sh").assert().success();
+fn proptest_urls_https() {
+    Command::new("tests/proptest_urls.sh").args(&["https"]).assert().success();
+}
+
+#[test]
+#[ignore]
+fn proptest_urls_ssh() {
+    Command::new("tests/proptest_urls.sh").args(&["ssh"]).assert().success();
 }
