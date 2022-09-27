806f3f11cdd9457e46b698414e74ed4861800861
Samuel E. Moelius III <samuel.moelius@trailofbits.com>
2022-09-27 11:08:46 -0400

    Expand CI tests

--- tests/ci.rs
+++ tests/ci.rs
@@ -2,7 +2,12 @@
 
 use assert_cmd::Command;
 use regex::Regex;
-use std::{fs::read_to_string, path::Path, str::from_utf8};
+use std::{
+    fs::read_to_string,
+    io::{stderr, Write},
+    path::Path,
+    str::from_utf8,
+};
 use tempfile::tempdir;
 
 #[test]
@@ -31,8 +36,15 @@ fn dylint() {
 }
 
 #[test]
+fn format() {
+    preserves_cleanliness(|| {
+        Command::new("cargo").arg("fmt").assert().success();
+    });
+}
+
+#[test]
 fn license() {
-    let re = Regex::new(r"^[^:]*\b(Apache-2.0|BSD-\d-Clause|CC0-1.0|MIT)\b").unwrap();
+    let re = Regex::new(r"^[^:]*\b(Apache-2.0|0BSD|BSD-\d-Clause|CC0-1.0|MIT)\b").unwrap();
 
     for line in std::str::from_utf8(
         &Command::new("cargo")
@@ -121,4 +133,34 @@ fn udeps() {
         .args(&["+nightly", "udeps", "--all-targets"])
         .assert()
         .success();
+}
+
+#[test]
+fn update() {
+    preserves_cleanliness(|| {
+        Command::new("cargo")
+            .args(["update", "--workspace"])
+            .assert()
+            .success();
+    });
+}
+
+fn preserves_cleanliness(f: impl FnOnce()) {
+    if dirty() {
+        #[allow(clippy::explicit_write)]
+        writeln!(stderr(), "Skipping as repository is dirty").unwrap();
+        return;
+    }
+
+    f();
+
+    assert!(!dirty());
+}
+
+fn dirty() -> bool {
+    Command::new("git")
+        .args(&["diff", "--exit-code"])
+        .assert()
+        .try_success()
+        .is_err()
 }
