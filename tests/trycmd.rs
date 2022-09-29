8f5b5c220bee1df185513d6d14b519d189169a32
Samuel Moelius <sam@moeli.us>
2022-09-28 20:34:54 -0400

    Add `trycmd` sanity tests

--- tests/trycmd.rs
+++ tests/trycmd.rs
@@ -2,7 +2,13 @@
 
 use assert_cmd::prelude::*;
 use necessist::util;
-use std::{path::PathBuf, process::Command};
+use regex::Regex;
+use std::{
+    ffi::OsStr,
+    fs::{read_dir, read_to_string},
+    path::PathBuf,
+    process::Command,
+};
 use trycmd::TestCases;
 
 const ROOT: &str = "examples/basic";
@@ -21,4 +27,81 @@ fn trycmd() {
     let _remove_file = util::RemoveFile(PathBuf::from(ROOT).join("necessist.db"));
 
     TestCases::new().case("tests/necessist_db_present/*.toml");
+}
+
+#[test]
+fn check_stdout() {
+    let re = Regex::new(r#"\b[0-9]+\.[0-9]+s\b"#).unwrap();
+
+    let necessist_db_absent = read_dir("tests/necessist_db_absent").unwrap();
+    let necessist_db_present = read_dir("tests/necessist_db_present").unwrap();
+    for entry in necessist_db_absent.chain(necessist_db_present) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if path.extension() != Some(OsStr::new("stdout")) {
+            continue;
+        }
+
+        let contents = read_to_string(&path).unwrap();
+
+        assert!(!re.is_match(&contents), "{:?} matches", path);
+    }
+}
+
+#[test]
+fn check_toml() {
+    let necessist_db_absent = read_dir("tests/necessist_db_absent").unwrap();
+    let necessist_db_present = read_dir("tests/necessist_db_present").unwrap();
+    for entry in necessist_db_absent.chain(necessist_db_present) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if path.extension() != Some(OsStr::new("toml")) {
+            continue;
+        }
+
+        let contents = read_to_string(&path).unwrap();
+        let document = toml::from_str::<toml::Value>(&contents).unwrap();
+
+        let args = document
+            .as_table()
+            .and_then(|table| table.get("args"))
+            .and_then(toml::Value::as_array)
+            .and_then(|array| {
+                array
+                    .iter()
+                    .map(toml::Value::as_str)
+                    .collect::<Option<Vec<_>>>()
+            })
+            .unwrap();
+
+        if path.parent().unwrap().file_name() == Some(OsStr::new("no_necessist_db")) {
+            assert_eq!(Some(&"--no-sqlite"), args.first());
+        }
+
+        let file_stem = &*path.file_stem().unwrap().to_string_lossy();
+        let example = file_stem.split_once('_').map_or(file_stem, |(s, _)| s);
+        assert!(args.contains(&format!("--root=examples/{}", example).as_str()));
+
+        let bin_name = document
+            .as_table()
+            .and_then(|table| table.get("bin"))
+            .and_then(toml::Value::as_table)
+            .and_then(|table| table.get("name"))
+            .and_then(toml::Value::as_str)
+            .unwrap();
+        assert_eq!(env!("CARGO_PKG_NAME"), bin_name);
+
+        let env_add_trycmd = document
+            .as_table()
+            .and_then(|table| table.get("env"))
+            .and_then(toml::Value::as_table)
+            .and_then(|table| table.get("add"))
+            .and_then(toml::Value::as_table)
+            .and_then(|table| table.get("TRYCMD"))
+            .and_then(toml::Value::as_str)
+            .unwrap();
+        assert_eq!("1", env_add_trycmd);
+    }
 }
