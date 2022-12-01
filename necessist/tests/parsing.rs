dec7d055e3d9cc35c187ae9a8f33c6143dbbd2d5
Samuel Moelius <sam@moeli.us>
2022-12-01 05:40:10 -0500

    Expand and improve parsing tests

--- necessist/tests/parsing.rs
+++ necessist/tests/parsing.rs
@@ -1,33 +1,60 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
-    fs::write,
+    fs::{remove_file, write},
     io::{stderr, BufRead, BufReader, Write},
 };
 use subprocess::{Exec, Redirection};
 use tempfile::tempdir;
 
-const TESTS: &[(bool, &str, Option<&str>, Option<&str>)] = &[
+struct Test {
+    expensive: bool,
+    url: &'static str,
+    subdir: Option<&'static str>,
+    framework_and_tomls: &'static [(Option<&'static str>, Option<&'static str>)],
+}
+
+const TESTS: &[Test] = &[
     // https://www.reddit.com/r/rust/comments/s6olun/comment/ht5l2kj
-    (false, "https://github.com/diem/diem", None, None),
+    Test {
+        expensive: false,
+        url: "https://github.com/diem/diem",
+        subdir: None,
+        framework_and_tomls: &[],
+    },
+    Test {
+        expensive: false,
+        url: "https://github.com/ProjectOpenSea/seaport",
+        subdir: None,
+        framework_and_tomls: &[(Some("hardhat-ts"), Some(SEAPORT_CONFIG))],
+    },
     // https://users.rust-lang.org/t/largest-rust-codebases/17027/7
-    (true, "https://github.com/rusoto/rusoto", None, None),
-    (
-        false,
-        "https://github.com/smartcontractkit/chainlink",
-        Some("contracts"),
-        Some(CHAINLINK_CONFIG),
-    ),
-    (
-        false,
-        "https://github.com/Uniswap/v3-core",
-        None,
-        Some(UNISWAP_CONFIG),
-    ),
+    Test {
+        expensive: true,
+        url: "https://github.com/rusoto/rusoto",
+        subdir: None,
+        framework_and_tomls: &[],
+    },
+    Test {
+        expensive: false,
+        url: "https://github.com/smartcontractkit/chainlink",
+        subdir: Some("contracts"),
+        framework_and_tomls: &[(Some("hardhat-ts"), Some(CHAINLINK_CONFIG))],
+    },
+    Test {
+        expensive: false,
+        url: "https://github.com/Uniswap/v3-core",
+        subdir: None,
+        framework_and_tomls: &[(None, Some(UNISWAP_CONFIG))],
+    },
 ];
 
 const CHAINLINK_CONFIG: &str = "\
 ignored_functions = [\"bigNumEquals\", \"evmRevert\"]
+";
+
+const SEAPORT_CONFIG: &str = "\
+ignored_functions = [\"checkExpectedEvents\"]
 ";
 
 const UNISWAP_CONFIG: &str = "\
@@ -44,11 +71,17 @@ lazy_static! {
 )]
 #[test]
 fn cheap_tests() {
-    for &(expensive, url, subdir, toml) in TESTS.iter() {
+    for &Test {
+        expensive,
+        url,
+        subdir,
+        framework_and_tomls,
+    } in TESTS.iter()
+    {
         if expensive {
             continue;
         }
-        run_test(url, subdir, toml);
+        run_test(url, subdir, framework_and_tomls);
     }
 }
 
@@ -59,83 +92,110 @@ fn cheap_tests() {
 #[test]
 #[ignore]
 fn all_tests() {
-    for &(_, url, subdir, toml) in TESTS.iter() {
-        run_test(url, subdir, toml);
+    for &Test {
+        expensive: _,
+        url,
+        subdir,
+        framework_and_tomls,
+    } in TESTS.iter()
+    {
+        run_test(url, subdir, framework_and_tomls);
     }
 }
 
-fn run_test(url: &str, subdir: Option<&str>, toml: Option<&str>) {
+fn run_test(url: &str, subdir: Option<&str>, framework_and_tomls: &[(Option<&str>, Option<&str>)]) {
     let tempdir = tempdir().unwrap();
 
+    #[allow(clippy::explicit_write)]
+    writeln!(stderr()).unwrap();
+
     assert!(Exec::cmd("git")
-        .args(&["clone", url, &tempdir.path().to_string_lossy()])
+        .args(&["clone", "--depth=1", url, &tempdir.path().to_string_lossy()])
         .stdout(Redirection::Merge)
         .stderr(Redirection::Merge)
         .join()
         .unwrap()
         .success());
 
-    let tomls = toml.map_or(vec![None], |toml| vec![None, Some(toml)]);
+    let framework_and_tomls = if framework_and_tomls.is_empty() {
+        &[(None, None)]
+    } else {
+        assert!(framework_and_tomls
+            .iter()
+            .all(|(framework, toml)| framework.is_some() || toml.is_some()));
+        framework_and_tomls
+    };
 
-    let mut candidates_prev = None;
+    for (framework, toml) in framework_and_tomls {
+        let tomls = toml.map_or(vec![None], |toml| vec![None, Some(toml)]);
 
-    for toml in tomls {
-        #[allow(clippy::explicit_write)]
-        writeln!(
-            stderr(),
-            "{}{}",
-            url,
-            if toml.is_some() {
-                " (with necessist.toml)"
+        let mut candidates_prev = None;
+
+        for toml in tomls {
+            #[allow(clippy::explicit_write)]
+            writeln!(
+                stderr(),
+                "
+{}{}{}",
+                url,
+                if let Some(framework) = framework {
+                    format!(" (with framework `{})", framework)
+                } else {
+                    String::new()
+                },
+                if toml.is_some() {
+                    " (with necessist.toml)"
+                } else {
+                    ""
+                }
+            )
+            .unwrap();
+
+            let root = subdir.map_or_else(
+                || tempdir.path().to_path_buf(),
+                |subdir| tempdir.path().join(subdir),
+            );
+
+            let necessist_toml = root.join("necessist.toml");
+            if let Some(toml) = toml {
+                write(necessist_toml, toml).unwrap();
             } else {
-                ""
+                remove_file(necessist_toml).unwrap_or_default();
             }
-        )
-        .unwrap();
 
-        let root = subdir.map_or_else(
-            || tempdir.path().to_path_buf(),
-            |subdir| tempdir.path().join(subdir),
-        );
+            let line = {
+                let mut exec = Exec::cmd("../target/debug/necessist");
+                exec = exec.args(&[
+                    "--no-sqlite",
+                    "--root",
+                    &root.to_string_lossy(),
+                    "--allow=config-files-experimental",
+                ]);
+                if let Some(framework) = framework {
+                    exec = exec.args(&["--framework", framework]);
+                }
+                exec = exec.stdout(Redirection::Pipe);
+                exec = exec.stderr(Redirection::Merge);
 
-        if let Some(toml) = toml {
-            write(root.join("necessist.toml"), toml).unwrap();
+                let mut popen = exec.popen().unwrap();
+                let stdout = popen.stdout.as_ref().unwrap();
+                let reader = BufReader::new(stdout);
+                let line = reader.lines().next().unwrap().unwrap();
+
+                #[allow(clippy::explicit_write)]
+                writeln!(stderr(), "{}", line).unwrap();
+
+                popen.kill().unwrap_or_default();
+                line
+            };
+
+            let captures = LINE_RE.captures(&line).unwrap();
+            assert!(captures.len() == 3);
+            let candidates_curr = captures[1].parse::<usize>().unwrap();
+            if let Some(candidates_prev) = candidates_prev {
+                assert!(candidates_prev > candidates_curr);
+            }
+            candidates_prev = Some(candidates_curr);
         }
-
-        let line = {
-            let mut exec = Exec::cmd("../target/debug/necessist");
-            exec = exec.args(&["--no-sqlite", "--root", &root.to_string_lossy()]);
-            exec = exec.stdout(Redirection::Pipe);
-            exec = exec.stderr(Redirection::Merge);
-
-            let mut popen = exec.popen().unwrap();
-            let stdout = popen.stdout.as_ref().unwrap();
-            let reader = BufReader::new(stdout);
-            let line = reader
-                .lines()
-                .find_map(|line| {
-                    let line = line.unwrap();
-
-                    #[allow(clippy::explicit_write)]
-                    writeln!(stderr(), "{}", line).unwrap();
-
-                    match line.as_ref() {
-                        "Warning: Configuration files are experimental"
-                        | "Silence this warning with: --allow config-files-experimental" => None,
-                        _ => Some(line),
-                    }
-                })
-                .unwrap();
-            popen.kill().unwrap_or_default();
-            line
-        };
-
-        let captures = LINE_RE.captures(&line).unwrap();
-        assert!(captures.len() == 3);
-        let candidates_curr = captures[1].parse::<usize>().unwrap();
-        if let Some(candidates_prev) = candidates_prev {
-            assert!(candidates_prev > candidates_curr);
-        }
-        candidates_prev = Some(candidates_curr);
     }
 }
