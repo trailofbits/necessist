9ad278a67b3702a923b50a2e70c6e712701140bc
Samuel Moelius <samuel.moelius@trailofbits.com>
2022-12-04 20:44:48 +0000

    Bump version

---
+++ scripts/update_versions.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# set -x
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "$0: expect one argument: version" >&2
+    exit 1
+fi
+
+VERSION="version = \"$1\""
+
+SCRIPTS="$(dirname "$(realpath "$0")")"
+WORKSPACE="$(realpath "$SCRIPTS"/..)"
+
+cd "$WORKSPACE"
+
+find . -name Cargo.toml ! -path './examples/*' -exec sed -i "{
+s/^version = \"[^\"]*\"$/$VERSION/
+}" {} \;
+
+REQ="${VERSION/\"/\"=}"
+
+find . -name Cargo.toml -exec sed -i "/^necessist/{
+s/^\(.*\)\<version = \"[^\"]*\"\(.*\)$/\1$REQ\2/
+}" {} \;
+
+scripts/update_lockfiles.sh
