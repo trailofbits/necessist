trailofbits/necessist

commit a21ea535203a970e3ecef5fb34b25a7afbf3e9e8

Rework warnings

core/src/warn.rs
@@ -4,9 +4,10 @@ use ansi_term::{
     Style,
 };
 use anyhow::{bail, Result};
+use bitflags::bitflags;
 use heck::ToKebabCase;
 use lazy_static::lazy_static;
-use std::{collections::BTreeSet, sync::Mutex};
+use std::{collections::BTreeMap, sync::Mutex};
 
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
@@ -29,26 +30,43 @@ impl std::fmt::Display for Warning {
     }
 }
 
-pub(crate) fn source_warn(
+bitflags! {
+    pub struct Flags: u8 {
+        const ONCE = 1 << 0;
+    }
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub fn source_warn(
     context: &LightContext,
     warning: Warning,
     source: &dyn ToConsoleString,
     msg: &str,
+    flags: Flags,
 ) -> Result<()> {
-    warn_internal(context, warning, Some(source), msg, false)
+    warn_internal(context, warning, Some(source), msg, flags)
 }
 
-pub(crate) fn warn(context: &LightContext, warning: Warning, msg: &str) -> Result<()> {
-    warn_internal(context, warning, None, msg, false)
+pub fn warn(context: &LightContext, warning: Warning, msg: &str, flags: Flags) -> Result<()> {
+    warn_internal(context, warning, None, msg, flags)
 }
 
-pub(crate) fn warn_once(context: &LightContext, warning: Warning, msg: &str) -> Result<()> {
-    warn_internal(context, warning, None, msg, true)
+const BUG_MSG: &str = "
+
+This may indicate a bug in Necessist. Consider opening an issue at: \
+https://github.com/trailofbits/necessist/issues
+";
+
+bitflags! {
+    struct State: u8 {
+        const ALLOW_MSG_EMITTED = 1 << 0;
+        const BUG_MSG_EMITTED = 1 << 1;
+        const WARNING_EMITTED = 1 << 2;
+    }
 }
 
 lazy_static! {
-    static ref WARNINGS_SHOWN: Mutex<BTreeSet<Warning>> = Mutex::new(BTreeSet::new());
-    static ref ALLOW_MSG_SHOWN: Mutex<BTreeSet<Warning>> = Mutex::new(BTreeSet::new());
+    static ref WARNING_STATE_MAP: Mutex<BTreeMap<Warning, State>> = Mutex::new(BTreeMap::new());
 }
 
 fn warn_internal(
@@ -56,12 +74,28 @@ fn warn_internal(
     warning: Warning,
     source: Option<&dyn ToConsoleString>,
     msg: &str,
-    once: bool,
+    flags: Flags,
 ) -> Result<()> {
     assert_ne!(warning, Warning::All);
 
+    #[allow(clippy::unwrap_used)]
+    let mut warning_state_map = WARNING_STATE_MAP.lock().unwrap();
+
+    let state = warning_state_map
+        .entry(warning)
+        .or_insert_with(State::empty);
+
+    // smoelius: Append `BUG_MSG` to `msg` in case we have to `bail!`.
+    let msg = msg.to_owned()
+        + if may_be_bug(warning) && !state.contains(State::BUG_MSG_EMITTED) {
+            state.insert(State::BUG_MSG_EMITTED);
+            BUG_MSG
+        } else {
+            ""
+        };
+
     if context.opts.deny.contains(&Warning::All) || context.opts.deny.contains(&warning) {
-        bail!(msg.to_owned());
+        bail!(msg);
     }
 
     if context.opts.quiet
@@ -71,28 +105,19 @@ fn warn_internal(
         return Ok(());
     }
 
-    if once {
-        #[allow(clippy::unwrap_used)]
-        let mut warnings_shown = WARNINGS_SHOWN.lock().unwrap();
-        if warnings_shown.contains(&warning) {
-            return Ok(());
-        }
-        warnings_shown.insert(warning);
+    if flags.contains(Flags::ONCE) && state.contains(State::WARNING_EMITTED) {
+        return Ok(());
     }
 
-    let allow_msg = {
-        #[allow(clippy::unwrap_used)]
-        let mut allow_msg_shown = ALLOW_MSG_SHOWN.lock().unwrap();
-        if allow_msg_shown.contains(&warning) {
-            String::new()
-        } else {
-            allow_msg_shown.insert(warning);
-            format!(
-                "
+    let allow_msg = if state.contains(State::ALLOW_MSG_EMITTED) {
+        String::new()
+    } else {
+        state.insert(State::ALLOW_MSG_EMITTED);
+        format!(
+            "
 Silence this warning with: --allow {}",
-                warning
-            )
-        }
+            warning
+        )
     };
 
     (context.println)(&format!(
@@ -129,4 +154,16 @@ pub(crate) fn note(context: &LightContext, msg: &str) {
         .paint("Note"),
         msg
     ));
+}
+
+fn may_be_bug(warning: Warning) -> bool {
+    match warning {
+        Warning::All => unreachable!(),
+        Warning::ConfigFilesExperimental
+        | Warning::DryRunFailed
+        | Warning::FilesChanged
+        | Warning::IgnoredFunctionsUnsupported
+        | Warning::IgnoredMacrosUnsupported => false,
+        Warning::ModulePathUnknown | Warning::RunTestFailed => true,
+    }
 }

core/Cargo.toml
@@ -13,6 +13,7 @@ repository = "https://github.com/trailofbits/necessist"
 ansi_term = "0.12"
 anyhow = "1.0"
 atty = "0.2"
+bitflags = "1.3"
 clap = { version = "4.0", features = ["cargo", "derive"], optional = true }
 ctrlc = "3.2"
 diesel = { version = "2.0", features = ["sqlite"] }

core/src/core.rs
@@ -1,7 +1,7 @@
 use crate::{
     framework::{self, ToImplementation},
-    note, source_warn, sqlite, util, warn, warn_once, Backup, Outcome, Rewriter, SourceFile, Span,
-    ToConsoleString, Warning,
+    note, source_warn, sqlite, util, warn, Backup, Outcome, Rewriter, SourceFile, Span,
+    ToConsoleString, WarnFlags, Warning,
 };
 use ansi_term::Style;
 use anyhow::{anyhow, bail, ensure, Result};
@@ -256,6 +256,7 @@ fn run(
                     Warning::DryRunFailed,
                     &test_file,
                     &format!("dry run failed: {}", error),
+                    WarnFlags::empty(),
                 )?;
             }
 
@@ -338,6 +339,7 @@ fn default_config(context: &LightContext, root: &Path) -> Result<()> {
         context,
         Warning::ConfigFilesExperimental,
         "Configuration files are experimental",
+        WarnFlags::empty(),
     )?;
 
     let toml = toml::to_string(&Config::default())?;
@@ -356,6 +358,7 @@ fn read_config(context: &LightContext, root: &Path) -> Result<Config> {
         context,
         Warning::ConfigFilesExperimental,
         "Configuration files are experimental",
+        WarnFlags::empty(),
     )?;
 
     let contents = read_to_string(path)?;
@@ -443,10 +446,11 @@ where
 
 fn update_progress(context: &Context, mismatch: bool, n: usize) -> Result<()> {
     if mismatch {
-        warn_once(
+        warn(
             &context.light(),
             Warning::FilesChanged,
             "Configuration or test files have changed since necessist.db was created",
+            WarnFlags::ONCE,
         )?;
     }
 

core/src/framework/impls/hardhat_ts/mod.rs
@@ -1,5 +1,5 @@
 use super::super::{Interface, Postprocess};
-use crate::{util, warn_once, Config, LightContext, Span, Warning};
+use crate::{util, warn, Config, LightContext, Span, WarnFlags, Warning};
 use anyhow::{anyhow, ensure, Context, Result};
 use log::debug;
 use std::{
@@ -141,10 +141,11 @@ impl Interface for HardhatTs {
 
 fn check_config(context: &LightContext, config: &Config) -> Result<()> {
     if !config.ignored_macros.is_empty() {
-        warn_once(
+        warn(
             context,
             Warning::IgnoredMacrosUnsupported,
             "The hardhat-ts framework does not support the `ignored_macros` configuration",
+            WarnFlags::ONCE,
         )?;
     }
 

core/src/framework/impls/rust/mod.rs
@@ -1,5 +1,5 @@
 use super::super::{Interface, Postprocess};
-use crate::{source_warn, util, warn_once, Config, LightContext, Span, TryInsert, Warning};
+use crate::{source_warn, util, warn, Config, LightContext, Span, TryInsert, WarnFlags, Warning};
 use anyhow::{anyhow, ensure, Context, Result};
 use cargo_metadata::Package;
 use log::debug;
@@ -11,7 +11,6 @@ use std::{
     path::{Path, PathBuf},
     process::{Command, Stdio},
     rc::Rc,
-    sync::atomic::{AtomicBool, Ordering},
 };
 use subprocess::{Exec, NullFile, Redirection};
 use syn::parse_file;
@@ -22,14 +21,6 @@ use parsing::{cached_test_file_fs_module_path, cached_test_file_package, Parsing
 
 mod visitor;
 use visitor::visit;
-
-const BUG_MSG: &str = "
-
-This may indicate a bug in Necessist. Consider opening an issue at: \
-https://github.com/trailofbits/necessist/issues
-";
-
-static BUG_MSG_SHOWN: AtomicBool = AtomicBool::new(false);
 
 #[derive(Debug)]
 pub struct Rust {
@@ -182,12 +173,13 @@ impl Interface for Rust {
                 if running_1_test {
                     return Ok(true);
                 }
-                let mut msg = format!("Failed to run test `{}`", test);
-                if !BUG_MSG_SHOWN.load(Ordering::SeqCst) {
-                    BUG_MSG_SHOWN.store(true, Ordering::SeqCst);
-                    msg += BUG_MSG;
-                }
-                source_warn(context, Warning::RunTestFailed, &span, &msg)?;
+                source_warn(
+                    context,
+                    Warning::RunTestFailed,
+                    &span,
+                    &format!("Failed to run test `{}`", test),
+                    WarnFlags::empty(),
+                )?;
                 Ok(false)
             })),
         )))
@@ -196,10 +188,11 @@ impl Interface for Rust {
 
 fn check_config(context: &LightContext, config: &Config) -> Result<()> {
     if !config.ignored_functions.is_empty() {
-        warn_once(
+        warn(
             context,
             Warning::IgnoredFunctionsUnsupported,
             "The rust framework does not currently support the `ignored_functions` configuration",
+            WarnFlags::ONCE,
         )?;
     }
 

core/src/framework/impls/rust/visitor.rs
@@ -1,10 +1,9 @@
 use super::{cached_test_file_fs_module_path, Parsing, Rust};
-use crate::{warn, Config, LightContext, SourceFile, Span, ToInternalSpan, Warning};
+use crate::{warn, Config, LightContext, SourceFile, Span, ToInternalSpan, WarnFlags, Warning};
 use anyhow::{Error, Result};
 use std::{
     path::{Path, PathBuf, StripPrefixError},
     rc::Rc,
-    sync::atomic::{AtomicBool, Ordering},
 };
 use syn::{
     punctuated::Punctuated,
@@ -12,8 +11,6 @@ use syn::{
     visit::{visit_expr_method_call, visit_item_fn, visit_item_mod, visit_stmt, Visit},
     Expr, ExprMacro, ExprMethodCall, File, Ident, ItemFn, ItemMod, Macro, PathSegment, Stmt, Token,
 };
-
-static BUG_MSG_SHOWN: AtomicBool = AtomicBool::new(false);
 
 #[cfg_attr(
     dylint_lib = "non_local_effect_before_error_return",
@@ -182,12 +179,12 @@ where
                 Ok(test_path) => test_path,
                 Err(error) => {
                     if error.downcast_ref::<StripPrefixError>().is_some() {
-                        let mut msg = format!("Failed to determine module path: {}", error);
-                        if !BUG_MSG_SHOWN.load(Ordering::SeqCst) {
-                            BUG_MSG_SHOWN.store(true, Ordering::SeqCst);
-                            msg += super::BUG_MSG;
-                        }
-                        warn(self.context, Warning::ModulePathUnknown, &msg)?;
+                        warn(
+                            self.context,
+                            Warning::ModulePathUnknown,
+                            &format!("Failed to determine module path: {}", error),
+                            WarnFlags::empty(),
+                        )?;
                     }
                     return Ok(());
                 }

core/src/lib.rs
@@ -48,5 +48,5 @@ use try_insert::TryInsert;
 pub mod util;
 
 mod warn;
-pub use warn::Warning;
-use warn::{note, source_warn, warn, warn_once};
+use warn::note;
+pub use warn::{source_warn, warn, Flags as WarnFlags, Warning};

core/src/to_console_string.rs
@@ -1,3 +1,3 @@
-pub(crate) trait ToConsoleString {
+pub trait ToConsoleString {
     fn to_console_string(&self) -> String;
 }

commit 2ea07fd6376b4e72b2d2bb0ee6f16326fff913d0

Fix bug introduced by a21ea535

core/src/warn.rs
@@ -101,11 +101,8 @@ fn warn_internal(
     if context.opts.quiet
         || context.opts.allow.contains(&Warning::All)
         || context.opts.allow.contains(&warning)
+        || (flags.contains(Flags::ONCE) && state.contains(State::WARNING_EMITTED))
     {
-        return Ok(());
-    }
-
-    if flags.contains(Flags::ONCE) && state.contains(State::WARNING_EMITTED) {
         return Ok(());
     }
 
@@ -135,6 +132,8 @@ Silence this warning with: --allow {}",
         msg,
         allow_msg
     ));
+
+    state.insert(State::WARNING_EMITTED);
 
     Ok(())
 }