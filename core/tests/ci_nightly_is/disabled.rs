1567baa7c4fb8bdf5778f0c8ca834fe7dcd2c6db
Samuel E. Moelius III <samuel.moelius@trailofbits.com>
2022-12-28 07:50:07 -0500

    Disable `udeps`
    It doesn't work with the build script: https://github.com/est31/cargo-udeps/issues/97

--- core/tests/ci_nightly_is/disabled.rs
+++
@@ -1,7 +0,0 @@
-use std::io::{stderr, Write};
-
-#[test]
-fn ci_nightly_is_disabled() {
-    #[allow(clippy::explicit_write)]
-    writeln!(stderr(), "Warning: feature `ci_nightly` is disabled").unwrap();
-}
--- core/tests/ci_nightly_is/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(not(feature = "ci_nightly"))]
-mod disabled;
-
-#[cfg(feature = "ci_nightly")]
-mod enabled;
