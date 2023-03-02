trailofbits/necessist

commit 2c841e20bec339ac79e5cb672660945cc4a3822a

Fix adjacent code

core/src/framework/impls/rust/mod.rs
@@ -48,7 +48,6 @@ impl Rust {
 }
 
 impl Interface for Rust {
-    #[allow(clippy::similar_names)]
     #[cfg_attr(
         dylint_lib = "non_local_effect_before_error_return",
         allow(non_local_effect_before_error_return)
@@ -72,7 +71,7 @@ impl Interface for Rust {
             assert!(test_file.is_absolute());
             assert!(test_file.starts_with(context.root));
             let content = read_to_string(test_file)?;
-            #[allow(clippy::unwrap_used)]
+            #[allow(clippy::similar_names, clippy::unwrap_used)]
             let file = parse_file(&content).with_context(|| {
                 format!(
                     "Failed to parse {:?}",

core/src/framework/mod.rs
@@ -21,8 +21,8 @@ pub type AutoUnion<T, U> = Auto<Union<T, U>>;
 pub type Postprocess = dyn Fn(&LightContext, Popen) -> Result<bool>;
 
 pub trait Interface: std::fmt::Debug {
-    #[allow(clippy::unwrap_used)]
     fn name(&self) -> String {
+        #[allow(clippy::unwrap_used)]
         let (_, type_name) = type_name::<Self>().rsplit_once("::").unwrap();
         type_name.to_kebab_case()
     }

necessist/src/framework/impls/foundry/mod.rs
@@ -53,11 +53,11 @@ impl Interface for Foundry {
 
         let mut spans = Vec::new();
 
-        #[allow(clippy::unwrap_used)]
         let mut visit_test_file = |test_file: &Path| -> Result<()> {
             assert!(test_file.is_absolute());
             assert!(test_file.starts_with(context.root));
             let contents = read_to_string(test_file)?;
+            #[allow(clippy::unwrap_used)]
             let (mut source_unit, _comments) = solang_parser::parse(&contents, 0)
                 .map_err(|error| anyhow!(format!("{error:?}")))
                 .with_context(|| {

core/src/backup.rs
@@ -5,6 +5,7 @@
 // smoelius: Allow `unwrap_used` until the following issue is resolved:
 // https://github.com/rust-lang/rust-clippy/issues/10264
 #![allow(clippy::unwrap_used)]
+#![cfg_attr(dylint_lib = "overscoped_allow", allow(overscoped_allow))]
 
 use std::{
     io::Result,
@@ -72,9 +73,9 @@ fn manual_copy(from: &Path, to: &Path) -> Result<()> {
     std::fs::write(to, contents)
 }
 
-#[allow(clippy::expect_used)]
 fn sibling_tempfile(path: &Path) -> Result<NamedTempFile> {
     let canonical_path = path.canonicalize()?;
+    #[allow(clippy::expect_used)]
     let parent = canonical_path
         .parent()
         .expect("should not fail for a canonical path");

core/src/framework/impls/rust/visitor.rs
@@ -350,8 +350,6 @@ impl ToInternalSpan for proc_macro2::Span {
 
 #[cfg(test)]
 mod test {
-    #![allow(clippy::panic)]
-
     use super::{IGNORED_MACROS, IGNORED_METHODS};
     use if_chain::if_chain;
     use std::fs::read_to_string;
@@ -405,6 +403,7 @@ mod test {
     fn ignored_methods_match_unnecessary_conversion_for_trait_watched_methods() {
         let data = get(UNNECESSARY_CONVERSION_FOR_TRAIT_URL).unwrap();
         let contents = std::str::from_utf8(&data).unwrap();
+        #[allow(clippy::panic)]
         let file = parse_file(contents).unwrap_or_else(|_| panic!("Failed to parse: {contents:?}"));
         let mut watched_methods = file
             .items

necessist/src/framework/impls/foundry/visitor.rs
@@ -21,7 +21,6 @@ use thiserror::Error;
 #[error(transparent)]
 struct Error(anyhow::Error);
 
-#[allow(clippy::unwrap_used)]
 pub(super) fn visit(
     framework: &mut Foundry,
     root: Rc<PathBuf>,
@@ -30,6 +29,7 @@ pub(super) fn visit(
     source_unit: &mut SourceUnit,
 ) -> Vec<Span> {
     let mut visitor = Visitor::new(framework, root, test_file, contents);
+    #[allow(clippy::unwrap_used)]
     visitor.visit_source_unit(source_unit).unwrap();
     visitor.spans
 }

commit 25f2d099424472633f1be7ed90f0dfe76b37957a

Run Clippy with `--warn=clippy::pedantic` in CI
Also, fix bug introduced by #214

core/src/framework/impls/rust/mod.rs
@@ -48,10 +48,12 @@ impl Rust {
 }
 
 impl Interface for Rust {
+    #[allow(clippy::similar_names)]
     #[cfg_attr(
         dylint_lib = "non_local_effect_before_error_return",
         allow(non_local_effect_before_error_return)
     )]
+    #[cfg_attr(dylint_lib = "overscoped_allow", allow(overscoped_allow))]
     fn parse(
         &mut self,
         context: &LightContext,
@@ -71,7 +73,7 @@ impl Interface for Rust {
             assert!(test_file.is_absolute());
             assert!(test_file.starts_with(context.root));
             let content = read_to_string(test_file)?;
-            #[allow(clippy::similar_names, clippy::unwrap_used)]
+            #[allow(clippy::unwrap_used)]
             let file = parse_file(&content).with_context(|| {
                 format!(
                     "Failed to parse {:?}",

scripts/clippy.sh
@@ -8,10 +8,11 @@ if [[ $# -ne 0 ]]; then
     exit 1
 fi
 
-# smoelius: The next command should match the `clippy` test in tests/ci_is/enabled.rs.
+# smoelius: The next command should match the `clippy` test in core/tests/ci_is/enabled.rs.
 
-cargo clippy --all-features --all-targets -- \
+cargo +nightly clippy --all-features --all-targets -- \
     -D warnings \
     -W clippy::pedantic \
+    -A clippy::let-underscore-untyped \
     -A clippy::missing-errors-doc \
     -A clippy::missing-panics-doc

core/tests/ci_is/enabled.rs
@@ -19,7 +19,9 @@ fn initialize() {
 
 #[test]
 fn clippy() {
-    clippy_command(&[], &["--deny=warnings"]).assert().success();
+    clippy_command(&[], &["--deny=warnings", "--warn=clippy::pedantic"])
+        .assert()
+        .success();
 }
 
 #[test]
@@ -173,6 +175,7 @@ fn update() {
 }
 
 fn clippy_command(cargo_args: &[&str], rustc_args: &[&str]) -> Command {
+    // smoelius: The next command should match what's in scripts/clippy.sh.
     let mut command = Command::new("cargo");
     command
         .args(["+nightly", "clippy", "--all-features", "--all-targets"])
@@ -180,6 +183,7 @@ fn clippy_command(cargo_args: &[&str], rustc_args: &[&str]) -> Command {
         .args(["--"])
         .args(rustc_args)
         .args([
+            "--allow=clippy::let-underscore-untyped",
             "--allow=clippy::missing-errors-doc",
             "--allow=clippy::missing-panics-doc",
         ]);