7f878542966f1b788c6d8a8e61a35104d83c927f
Samuel Moelius <sam@moeli.us>
2023-03-18 12:10:42 -0400

    Add go_test_ratio.sh script

---
+++ scripts/go_test_ratio.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+rm -rf ~/.cache/go-build
+
+grep -l -r -w defer --include=*_test.go |
+xargs -n 1 dirname |
+sort -u |
+while read X; do
+    Y="$(go test ./"$X" | grep -m 1 '^ok\>')"
+    if [[ -z "$Y" || "$Y" =~ cached ]]; then
+        continue
+    fi
+    N="$(grep -I '^func Test' "$X"/*_test.go | wc -l)"
+    MILLIS="$(echo "$Y" | sed 's/^.*[[:space:]]\([0-9]\+\)\.\([0-9]\{3\}\)s$/\1\2/;s/^0*//')"
+    echo "$X" $(($N*1000/$MILLIS))
+done |
+tee /dev/tty |
+sort -k 2 -n
