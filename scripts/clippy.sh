030d3437b77dd61778d36aa6129d428596cb41b2
Samuel Moelius <sam@moeli.us>
2023-06-09 18:05:39 -0400

    Accept arguments in clippy.sh

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -3,14 +3,9 @@
 # set -x
 set -euo pipefail
 
-if [[ $# -ne 0 ]]; then
-    echo "$0: expect no arguments" >&2
-    exit 1
-fi
-
 # smoelius: The next command should match the `clippy` test in core/tests/ci_is/enabled.rs.
 
-cargo +nightly clippy --all-features --all-targets -- \
+cargo +nightly clippy --all-features --all-targets "$@" -- \
     -D warnings \
     -W clippy::pedantic \
     -A clippy::let-underscore-untyped \
