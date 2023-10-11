251a072b4adac4a518e29a28e863b351b9086701
Samuel Moelius <sam@moeli.us>
2023-10-11 05:48:20 -0400

    Update clippy.sh

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -5,9 +5,13 @@ set -euo pipefail
 
 # smoelius: The next command should match the `clippy` test in necessist/tests/ci.rs.
 
+# smoelius: Allow `iter-without-into-iter` until the following issue is resolved:
+# https://github.com/bitflags/bitflags/issues/379
+
 cargo +nightly clippy --all-features --all-targets "$@" -- \
     -D warnings \
     -W clippy::pedantic \
     -A clippy::missing-errors-doc \
     -A clippy::missing-panics-doc \
-    -A clippy::needless_pass_by_ref_mut
+    -A clippy::needless_pass_by_ref_mut \
+    -A clippy::iter-without-into-iter
