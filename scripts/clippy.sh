b40174d7bad5b3a6e465ed8f7873ac952a01f190
Samuel Moelius <samuel.moelius@trailofbits.com>
2023-10-23 13:04:29 +0000

    Make Clippy happy

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -5,13 +5,9 @@ set -euo pipefail
 
 # smoelius: The next command should match the `clippy` test in necessist/tests/ci.rs.
 
-# smoelius: Allow `iter-without-into-iter` until the following issue is resolved:
-# https://github.com/bitflags/bitflags/issues/379
-
 cargo +nightly clippy --all-features --all-targets "$@" -- \
     -D warnings \
     -W clippy::pedantic \
     -A clippy::missing-errors-doc \
     -A clippy::missing-panics-doc \
-    -A clippy::needless_pass_by_ref_mut \
-    -A clippy::iter-without-into-iter
+    -A clippy::struct-field-names
