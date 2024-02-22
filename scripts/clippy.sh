3568f5d7097efc6107b2b99e09b74962b092a35b
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-02-22 15:58:32 +0000

    Add TOC test

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -9,6 +9,7 @@ cargo +nightly clippy --all-features --all-targets "$@" -- \
     -D warnings \
     -W clippy::pedantic \
     -W clippy::let_underscore_untyped \
+    -A clippy::fomrat-collect \
     -A clippy::missing-errors-doc \
     -A clippy::missing-panics-doc \
     -A clippy::struct-field-names
