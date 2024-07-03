3c0f7855a8e982c502d93ecbf11511bc2e66c1d2
Samuel Moelius <sam@moeli.us>
2024-07-03 06:05:35 -0400

    Fix `rerun-if-changed` instructions
    build.rs no longer exists.

--- backends/build/main.rs
+++ backends/build/main.rs
@@ -5,7 +5,5 @@ fn main() {
     solang::emit();
     syn::emit();
 
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=assets/solang_parser_pt.rs");
-    println!("cargo:rerun-if-changed=assets/syn_expr.rs");
+    println!("cargo:rerun-if-changed=assets");
 }
