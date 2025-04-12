7c1a009b0d4494f4440594df20742ef8639487ca
Samuel Moelius <35515885+smoelius@users.noreply.github.com>
2025-04-12 07:59:18 -0400

    Revert "Sort `walk_dir_results` in CI"
    This reverts commit 0ecc11b47735af18e8d78be251428558e4a493b1.

--- backends/build/main.rs
+++ backends/build/main.rs
@@ -2,15 +2,8 @@ mod solang;
 mod syn;
 
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(sort_walk_dir_results)");
-
-    if std::env::var_os("CI").is_some() {
-        println!("cargo::rustc-cfg=sort_walk_dir_results");
-    }
-
     solang::emit();
     syn::emit();
 
-    println!("cargo::rerun-if-env-changed=CI");
-    println!("cargo::rerun-if-changed=assets");
+    println!("cargo:rerun-if-changed=assets");
 }
