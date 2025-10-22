a8ac3c8190ca996252e5ca53c2c3fd3db0f19095
Samuel Moelius <35515885+smoelius@users.noreply.github.com>
2025-10-22 13:51:02 -0400

    Update third_party_2.rs

--- necessist/tests/third_party_2.rs
+++ necessist/tests/third_party_2.rs
@@ -3,7 +3,6 @@ mod third_party_common;
 const PATH: &str = "tests/third_party_tests/2";
 
 #[cfg_attr(dylint_lib = "general", allow(non_thread_safe_call_in_test))]
-#[ignore = "disabled until I can figure out why the test keeps failing"]
 #[test]
 fn all_tests() {
     third_party_common::all_tests_in(PATH);
