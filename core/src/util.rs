2a95c191593787df740ed75b7a992ba653fe9a80
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-02-17 15:32:29 -0500

    Make Clippy happy

--- core/src/util.rs
+++ core/src/util.rs
@@ -20,7 +20,7 @@ impl Drop for RemoveFile {
 
 /// Constructs a [`subprocess::Exec`] from a [`std::process::Command`].
 pub fn exec_from_command(command: &Command) -> Exec {
-    let mut exec = Exec::cmd(command.get_program()).args(&command.get_args().collect::<Vec<_>>());
+    let mut exec = Exec::cmd(command.get_program()).args(command.get_args().collect::<Vec<_>>());
     for (key, val) in command.get_envs() {
         if let Some(val) = val {
             exec = exec.env(key, val);
