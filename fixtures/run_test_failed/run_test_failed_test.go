e44397ba86d0c1001b92cef409d4b3ad8c900d07
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-06-26 08:08:03 -0400

    Change `cfg` Rust test to a Go-based test

--- fixtures/run_test_failed/run_test_failed_test.go
+++ fixtures/run_test_failed/run_test_failed_test.go
@@ -1,34 +1,16 @@
-#[cfg(feature = "passed")]
-#[test]
-fn passed() {
-    let mut n = 0;
-    n += 1;
-    noop();
+package run_test_failed
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestPassed(t *testing.T) {
+	fmt.Println("x")
+	fmt.Println("y")
 }
 
-fn noop() {}
-
-#[cfg(feature = "timed-out")]
-#[test]
-fn timed_out() {
-    let mut n = 0;
-    while n < 1 {
-        n += 1;
-    }
-}
-
-#[cfg(feature = "failed")]
-#[test]
-fn failed() {
-    let mut n = 0;
-    n += 1;
-    assert!(n >= 1);
-}
-
-#[cfg(feature = "nonbuildable")]
-#[test]
-fn nonbuildable() {
-    let _ = |xs: &[&str]| -> String {
-        return xs.join("");
-    };
+func TestMain(m *testing.M) {
+	os.Exit(0)
 }
